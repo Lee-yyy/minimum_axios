6c5e63f7eeb68da48b61dcb78ea8f6375f776f1f
Lee-yyy <940165771@qq.com>
2021-04-23 16:09:22 +0800

    feat: 合并配置——默认配置

--- src/axios.ts
+++ src/axios.ts
@@ -1,14 +1,15 @@
 import { deflate } from 'zlib'
 import Axios from './core/Axios'
+import defaults from './defaults'
 import { extend } from './helpers/util'
-import { AxiosInstance } from './types'
+import { AxiosInstance, AxiosRequestConfig } from './types'
 
-function createInstance(): AxiosInstance {
-  const context = new Axios()
+function createInstance(config: AxiosRequestConfig): AxiosInstance {
+  const context = new Axios(config)
   const instance = Axios.prototype.request.bind(context) //保证request函数在调用this时可以拿到
 
   extend(instance, context)
   return instance as AxiosInstance
 }
-const axios = createInstance()
+const axios = createInstance(defaults)
 export default axios
